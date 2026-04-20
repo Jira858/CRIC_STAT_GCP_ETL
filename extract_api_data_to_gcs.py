49d188b787c952fc3675e953b2609797c7d79e9c
rajalakshmim28 <48022934+Jira858@users.noreply.github.com>
2026-04-21 00:54:30 +0530

    Update and rename extract_api_data.py to extract_api_data_to_gcs.py

--- extract_api_data_to_gcs.py
+++ extract_api_data_to_gcs.py
@@ -1,9 +1,10 @@
 import requests
 import csv
+from google.cloud import storage
 
 url = 'https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen'
 headers = {
-     "X-RapidAPI-Key": "RapidAPI key"
+        "X-RapidAPI-Key": "RapidAPI key"  
     'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
 }
 params = {
@@ -22,13 +23,23 @@ if response.status_code == 200:
         # Write data to CSV file with only specified field names
         with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=field_names)
-            #writer.writeheader()
+            # writer.writeheader()
             for entry in data:
                 writer.writerow({field: entry.get(field) for field in field_names})
 
         print(f"Data fetched successfully and written to '{csv_filename}'")
+
+        # Upload the CSV file to GCS
+        bucket_name = 'bkt-ranking-data'
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        destination_blob_name = f'{csv_filename}'  # The path to store in GCS
+
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(csv_filename)
+
+        print(f"File {csv_filename} uploaded to GCS bucket {bucket_name} as {destination_blob_name}")
     else:
         print("No data available from the API.")
-
 else:
     print("Failed to fetch data:", response.status_code)
